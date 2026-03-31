1ed05ee9172d3dc56f2442658a25164665bc3d0a
gabrielharo-dotcom <gabriel.haro@tecsup.edu.pe>
2026-03-31 17:20:42 -0500

    Añadiendo Gomez

--- src/main/java/pe/edu/tecsup/lab03/Application.java
+++ src/main/java/pe/edu/tecsup/lab03/Application.java
@@ -5,6 +5,7 @@ public class Application {
     //  to do a  nhh
     // to do by gabriel se
     public static void main(String[] args) {
-        int jaime = 10;
+        int jaime = 10;;
+        int gomez = 11;
     }
 }
